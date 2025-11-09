f0478ea2f1507cc84e84e88eca46e980e168f09b
“ardiannnnnnn” <ardianindonesia45@gmail.com>
2025-11-09 19:41:48 +0700

    square15

---
+++ Jobsheet_8/squareNoAbsen.java
@@ -0,0 +1,24 @@
+package Jobsheet_8;
+
+import java.util.Scanner;
+
+public class squareNoAbsen {
+     public static void main(String[] args) {
+
+        @SuppressWarnings("resource")
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukan Nilai N = ");
+        int N = sc.nextInt();
+
+        
+        for (int iOuter = 1; iOuter <= N; iOuter++) {
+            for (int i = 1; i <= N; i++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+    }
+}
