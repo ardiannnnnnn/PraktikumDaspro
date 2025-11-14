59d809c58b44e71b76f4ecc926322fafddc291dd
“ardiannnnnnn” <ardianindonesia45@gmail.com>
2025-11-14 21:04:14 +0700

    modifikasi 2

--- Jobsheet_9/arrayNilai15.java
+++ Jobsheet_9/arrayNilai15.java
@@ -5,15 +5,21 @@ import java.util.Scanner;
 public class arrayNilai15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         int[] nilaiAkhir = new int[10];
 
-        for (int i = 0; i < 10; i++){
-            System.out.print("Masukkan Nilai Akhir ke-"+i+" : ");
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            System.out.print("Masukkan nilai akhir ke- " + i + " :");
             nilaiAkhir[i] = sc.nextInt();
         }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Masukkan Nilai Akhir ke - "+i+ "adalah " +nilaiAkhir[1]);
+
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-" + i + " dinyatakan Lulus");
+                System.out.println("Mahasiswa ke-" + i + " dinyatakan Tidak Lulus");
+            }
         }
+
+        sc.close();
     }
 }
