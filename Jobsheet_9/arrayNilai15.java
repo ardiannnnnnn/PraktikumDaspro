44149ddeaa29769dd5a07a8711688c9b57c28836
“ardiannnnnnn” <ardianindonesia45@gmail.com>
2025-11-14 21:05:20 +0700

    modifikasi 3

--- Jobsheet_9/arrayNilai15.java
+++ Jobsheet_9/arrayNilai15.java
@@ -6,19 +6,36 @@ public class arrayNilai15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] nilaiAkhir = new int[10];
+        int[] nilaiMhs = new int[10];
+        double total = 0, rata2;
+        int max, min;
 
-        for (int i = 0; i < nilaiAkhir.length; i++) {
-            System.out.print("Masukkan nilai akhir ke- " + i + " :");
-            nilaiAkhir[i] = sc.nextInt();
+        
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
+            nilaiMhs[i] = sc.nextInt();
         }
 
-        for (int i = 0; i < nilaiAkhir.length; i++) {
-            if (nilaiAkhir[i] > 70) {
-                System.out.println("Mahasiswa ke-" + i + " dinyatakan Lulus");
-                System.out.println("Mahasiswa ke-" + i + " dinyatakan Tidak Lulus");
+        max = nilaiMhs[0];
+        min = nilaiMhs[0];
+
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            total += nilaiMhs[i];
+
+            if (nilaiMhs[i] > max) {
+                max = nilaiMhs[i];
+            }
+
+            if (nilaiMhs[i] < min) {
+                min = nilaiMhs[i];
             }
         }
+
+        rata2 = total / nilaiMhs.length;
+
+        System.out.println("Rata-rata nilai mahasiswa = " + rata2);
+        System.out.println("Nilai tertinggi = " + max);
+        System.out.println("Nilai terendah = " + min);
 
         sc.close();
     }
