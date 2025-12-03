3b75ae4ba166bb20ca1f2440cb84c9fddc9c61a5
“ardiannnnnnn” <ardianindonesia45@gmail.com>
2025-12-03 08:24:39 +0700

    percobaan 4

---
+++ Jobsheet_12/pengunjungCafe15.java
@@ -0,0 +1,14 @@
+public class pengunjungCafe15 {
+    static void daftarPengunjung(String...namaPengunjung){
+        System.out.println("DAftar nama penguunjung: ");
+        for (int i = 0 ; i < namaPengunjung.length; i++) {
+            System.out.println("- "+namaPengunjung[i]);
+        }
+        
+    }
+    public static void main(String[] args) {
+        daftarPengunjung("Andi");
+        daftarPengunjung("Doni","Eti","Fahmi", "Galih");
+        
+    }
+}
