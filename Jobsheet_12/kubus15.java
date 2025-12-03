d570c521afcc394a2cc56e6c1ef31bffa89e6c03
“ardiannnnnnn” <ardianindonesia45@gmail.com>
2025-12-03 09:06:59 +0700

    tugas 1

---
+++ Jobsheet_12/kubus15.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class kubus15 {
+    
+    public static int volumeKubus(int sisi){
+        return sisi * sisi * sisi;
+    }
+
+    public static int luasPermukaanKubus(int sisi){
+        return 6 * sisi * sisi;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan panjang sisi kubus : ");
+        int sisi = sc.nextInt();
+
+        int volume = volumeKubus(sisi);
+        int luasPermukaan = luasPermukaanKubus(sisi);
+
+        System.out.println("Volume kubus : " + volume);
+        System.out.println("Luas permukaan kubus : "+ luasPermukaan);
+
+        sc.close();
+    }
+}
